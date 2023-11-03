ff72f82c4e9fd4370da4dc5e8c49013d16ff6b19
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-11-03 23:31:21 +0530

    api - join , create

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('room',RoomView.as_view())
+    path('room',RoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view())
+
     
 ]
